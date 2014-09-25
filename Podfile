72afea8f4f3f1d0e273e2b433a5a78021ba6e372
Shaun Merritt <smerrittdevelopment@gmail.com>
2014-09-25 16:59:41 -0700

    Added JNWSpringAnimation

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@ platform :ios, "7.1"
 pod 'GPUImage', '~> 0.1.0'
 pod 'JSONKit'
 pod 'pop', '~> 1.0'
+pod 'JNWSpringAnimation'
 
 target :AugmentedRealityWeatherApp, :exclusive => true do
   # pod 'Kiwi'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_GPUImage 1
 #define COCOAPODS_VERSION_PATCH_GPUImage 5
 
+// JNWSpringAnimation
+#define COCOAPODS_POD_AVAILABLE_JNWSpringAnimation
+#define COCOAPODS_VERSION_MAJOR_JNWSpringAnimation 0
+#define COCOAPODS_VERSION_MINOR_JNWSpringAnimation 5
+#define COCOAPODS_VERSION_PATCH_JNWSpringAnimation 0
+
 // JSONKit
 #define COCOAPODS_POD_AVAILABLE_JSONKit
 // This library does not follow semantic-versioning,
